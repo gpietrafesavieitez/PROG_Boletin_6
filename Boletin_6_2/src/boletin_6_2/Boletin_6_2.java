af24f4e21cba41ea11056e5d028af5706574e5c3
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2018-10-29 12:39:58 +0100

    Falta acabar

---
+++ Boletin_6_2/src/boletin_6_2/Boletin_6_2.java
@@ -0,0 +1,16 @@
+package boletin_6_2;
+
+import javax.swing.JOptionPane;
+
+public class Boletin_6_2 {
+
+    public static void main(String[] args) {
+        short num1 = Short.parseShort(JOptionPane.showInputDialog("Dame número 1: "));
+        short num2 = Short.parseShort(JOptionPane.showInputDialog("Dame número 2: "));
+        if(num1 > num2 || num1 == num2){
+            JOptionPane.showMessageDialog(null,"A resta de ambos números é " + (num1 - num2));
+           
+        }
+    }
+    
+}
