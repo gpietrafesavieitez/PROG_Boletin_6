2e5e058555efb987e48e347324f77fc0d0f59fd0
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2018-11-05 12:20:03 +0100

    Refactorizado.

--- Boletin_6_5/src/boletin_6_5/Boletin_6_5.java
+++ Boletin_6_5/src/boletin_6_5/Boletin_6_5.java
@@ -4,24 +4,22 @@ import javax.swing.JOptionPane;
 public class Boletin_6_5 {
 
     public static void main(String[] args) {
-        int n1,n2,n3;
         try{
-            n1 = Integer.parseInt(JOptionPane.showInputDialog("Dame un número:"));
-            n2 = Integer.parseInt(JOptionPane.showInputDialog("Dame otro distinto:"));
+            int n1 = Integer.parseInt(JOptionPane.showInputDialog("Dame un número:"));
+            int n2 = Integer.parseInt(JOptionPane.showInputDialog("Dame otro distinto:"));
             if(n1 == n2){
                 JOptionPane.showMessageDialog(null,"No son distintos.");
             }else{
-                n3 = Integer.parseInt(JOptionPane.showInputDialog("Dame otro más:"));
+                int n3 = Integer.parseInt(JOptionPane.showInputDialog("Dame otro más:"));
                 if(n3 == n1 || n3 == n2){
                     JOptionPane.showMessageDialog(null,"No son distintos.");
                 }else{
                     if(n1 > n2 && n1 > n3){
                         JOptionPane.showMessageDialog(null,"El primer número es el mayor de todos.");
-                    }
-                    if(n2 > n1 && n2 > n3){
+                    }else if(n2 > n1 && n2 > n3){
                         JOptionPane.showMessageDialog(null,"El segundo número es el mayor de todos.");
                     }
-                    if(n3 > n1 && n3 > n2){
+                    else{
                         JOptionPane.showMessageDialog(null,"El tercer número es el mayor de todos.");
                     }
                 }
